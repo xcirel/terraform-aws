a0a8e3e55a8d47247447705f2beacfc1b80c4e9c
Eric <eubdsantos@gmail.com>
2023-01-21 02:19:14 -0300

    specify version terraform and provider

--- main.tf
+++ main.tf
@@ -1,5 +1,17 @@
+terraform {
+  required_version = "1.3.7"
+
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "4.51.0"
+    }
+  }
+}
+
 provider "aws" {
-    region = "us-east-1"       
+    region = "us-east-1"     
+    profile = "terraform"  
 }
 
 resource "aws_s3_bucket" "my-test-bucket" {
