9cf4b661303a7cd70e1fbb5a2cd6c74999831c66
Eric <eubdsantos@gmail.com>
2023-01-21 10:02:16 -0300

    Testing change

---
+++ 02-change-destroy/main.tf
@@ -0,0 +1,28 @@
+terraform {
+  required_version = "1.3.7"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.51.0"
+    }
+  }
+}
+
+provider "aws" {
+  region  = "us-east-1"
+  profile = "terraform"
+}
+
+resource "aws_s3_bucket" "my-test-bucket" {
+  bucket = "my-tf-test-bucket-devops-lab-9043943867348987878986743"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+    ManagedBy   = "Terraform"
+    Owner       = "Eric dos Santos"
+    UpdateAt    = "2023-01-21"
+
+  }
+}
